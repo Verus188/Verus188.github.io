ed09e8f2a1048c9fa3294e71970fab54be13e1e1
Verus188 <89634774+Verus188@users.noreply.github.com>
2025-02-18 13:55:27 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,55 +1,43 @@
 console.log("script is working");
 
 
-var fig1 = document.getElementById("figure1");
-console.log(fig1);
+let fig = document.getElementById("figure");
 
+fig.addEventListener("mousedown", function(e) {
+    let reg = /-?\d{1,}/g;
 
-fig1.onmousedown = function(e) {
-    var counter = 0;
-    function moveAt(e) {
-        fig1.style.transform = `translate(${counter}px, 0px)`;
-        // fig1.style.transform = `translate(0px, ${counter}px)`;
-        console.log(e.pageX);
-        console.log(fig1);
+    let oldClientX = e.clientX;
+    let oldClientY = e.clientY;
+    
+
+    if (fig.style.translate == "") {
+        oldX = 0;
+        oldY = 0;
+    } else {
+        oldX = fig.style.translate.match(reg)[0]
+        oldY = fig.style.translate.match(reg)[1]
     }
-    fig1.style.position = "absolute";
-    moveAt(e);
-    // document.body.appendChild(fig1);
 
-    // fig1.style.zIndex = 1000;
 
+    function moveAt(e) {
+
+        let offsetX = oldClientX - e.clientX;
+        let offsetY = oldClientY - e.clientY;
+
+
+
+        fig.style.translate = `${oldX-offsetX}px ${oldY-offsetY}px`
+
+    }
 
     document.onmousemove = function(e) {
         moveAt(e);
     }
 
-    fig1.onmouseup = function() {
+    document.onmouseup = function() {
         document.onmousemove = null;
-        fig1.onmouseup = null;
+        fig.onmouseup = null;
+        
     }
-
-}
-
-// fig1.onmousedown = function(e) {
-//     function moveAt(e) {
-//         fig1.style.left = e.pageX - fig1.offsetWidth / 2 + "px";
-//         fig1.style.top = e.pageY - fig1.offsetHeight / 2 + "px";
-//     }
-//     fig1.style.position = "absolute";
-//     moveAt(e);
-//     // document.body.appendChild(fig1);
-
-//     // fig1.style.zIndex = 1000;
-
-
-//     document.onmousemove = function(e) {
-//         moveAt(e);
-//     }
-
-//     fig1.onmouseup = function() {
-//         document.onmousemove = null;
-//         fig1.onmouseup = null;
-//     }
-
-// }
+    
+});
