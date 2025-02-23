6a4f9ba3515d0089454b360c14f19cdf66f92b01
Verus188 <89634774+Verus188@users.noreply.github.com>
2025-02-23 12:14:11 +0300

    drag&drop for all figures

--- script.js
+++ script.js
@@ -1,43 +1,50 @@
 console.log("script is working");
 
 
-let fig = document.getElementById("figure");
+let figs = document.getElementsByClassName("figure");
 
-fig.addEventListener("mousedown", function(e) {
-    let reg = /-?\d{1,}/g;
 
-    let oldClientX = e.clientX;
-    let oldClientY = e.clientY;
+for (let i=0; i<figs.length; i++) {
+    let fig = figs[i];
+    console.log(fig);
     
 
-    if (fig.style.translate == "") {
-        oldX = 0;
-        oldY = 0;
-    } else {
-        oldX = fig.style.translate.match(reg)[0]
-        oldY = fig.style.translate.match(reg)[1]
-    }
+    fig.addEventListener("mousedown", function(e) {
+        let reg = /-?\d{1,}/g;
 
-
-    function moveAt(e) {
-
-        let offsetX = oldClientX - e.clientX;
-        let offsetY = oldClientY - e.clientY;
-
-
-
-        fig.style.translate = `${oldX-offsetX}px ${oldY-offsetY}px`
-
-    }
-
-    document.onmousemove = function(e) {
-        moveAt(e);
-    }
-
-    document.onmouseup = function() {
-        document.onmousemove = null;
-        fig.onmouseup = null;
+        let oldClientX = e.clientX;
+        let oldClientY = e.clientY;
         
-    }
-    
-});
+
+        if (fig.style.translate == "") {
+            oldX = 0;
+            oldY = 0;
+        } else {
+            oldX = fig.style.translate.match(reg)[0]
+            oldY = fig.style.translate.match(reg)[1]
+        }
+
+
+        function moveAt(e) {
+
+            let offsetX = oldClientX - e.clientX;
+            let offsetY = oldClientY - e.clientY;
+
+
+
+            fig.style.translate = `${oldX-offsetX}px ${oldY-offsetY}px`
+
+        }
+
+        document.onmousemove = function(e) {
+            moveAt(e);
+        }
+
+        document.onmouseup = function() {
+            document.onmousemove = null;
+            fig.onmouseup = null;
+            
+        }
+        
+    });
+}
