d5b9b0aba1a05230111cdafefed22fda3bc01568
Verus188 <king_king_03@bk.ru>
2025-02-25 20:43:51 +0300

    можно добавлять фигуры из панели справа

--- script.js
+++ script.js
@@ -1,38 +1,30 @@
+let slots_parts = new Map();
+slots_parts.set("1", '<circle class="figure" cx="100" cy="200" r="50" fill="darkgoldenrod"></circle>');
+slots_parts.set("2", '<rect class="figure" x="100" y="200" width="100" height="100" fill="darkgoldenrod"></rect>');
+slots_parts.set("3", '<path class="figure" d="M 100 200 h 100 l -50 -100 z" fill="darkgoldenrod"></path>');
+slots_parts.set("4", '<path class="figure" d="M 100 200 h 100 l 40 10 l 40 -10 v 80 l -40 -10 l -40 10 h -100 z" fill="darkgoldenrod"></path>');
+
 let figs = document.getElementsByClassName("figure");
+letElemsMove(figs);
 
-letFiguresMove(figs);
-
-let create_button = document.getElementById("b1");
 let svg_zone = document.getElementById("zone");
-create_button.onclick = function(e) {
 
 
-    // svg_zone.appendChild(fig);
-    svg_zone.innerHTML += '<path class="figure" d="M 400 350 h 100 l 40 10 l 40 -10 v 80 l -40 -10 l -40 10 h -100 z" fill="gold"></path>'
-    
-    figs = document.getElementsByClassName("figure");
-    letFiguresMove(figs);
-    
+function moveAble(elem) {
 
-
-    
-}
-
-function moveAble(fig) {
-
-    fig.addEventListener("mousedown", function(e) {
+    elem.addEventListener("mousedown", function(e) {
         let reg = /-?\d{1,}/g;
 
         let oldClientX = e.clientX;
         let oldClientY = e.clientY;
         
 
-        if (fig.style.translate == "") {
+        if (elem.style.translate == "") {
             oldX = 0;
             oldY = 0;
         } else {
-            oldX = fig.style.translate.match(reg)[0]
-            oldY = fig.style.translate.match(reg)[1]
+            oldX = elem.style.translate.match(reg)[0]
+            oldY = elem.style.translate.match(reg)[1]
         }
 
 
@@ -43,7 +35,7 @@ function moveAble(fig) {
 
 
 
-            fig.style.translate = `${oldX-offsetX}px ${oldY-offsetY}px`
+            elem.style.translate = `${oldX-offsetX}px ${oldY-offsetY}px`
 
         }
 
@@ -53,7 +45,7 @@ function moveAble(fig) {
 
         document.onmouseup = function() {
             document.onmousemove = null;
-            fig.onmouseup = null;
+            elem.onmouseup = null;
             
         }
         
@@ -61,10 +53,29 @@ function moveAble(fig) {
 
 }
 
-function letFiguresMove(figs) {
-    for (let i=0; i<figs.length; i++) {
-        let fig = figs[i];
+function letElemsMove(elems) {
+    for (let i=0; i<elems.length; i++) {
+        let elem = elems[i];
 
-        fig.addEventListener("mousedown", moveAble(fig));
+        elem.addEventListener("mousedown", moveAble(elem));
     }
 }
+
+let slots = document.getElementsByClassName("slot");
+
+for (let i=0; i<slots.length; i++) {
+    let slot = slots[i];
+
+    slot.addEventListener("click", function(e) {
+        let regex = /\d{1,}/g;
+
+        let slot_id = slot.getAttribute("id").match(regex)[0];
+        
+        svg_zone.innerHTML += slots_parts.get(slot_id);
+        created_part = svg_zone.lastChild;
+
+        let figs = document.getElementsByClassName("figure");
+        letElemsMove(figs);                
+
+    });
+};
